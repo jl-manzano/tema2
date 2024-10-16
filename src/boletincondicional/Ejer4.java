58a195e781ca7161da775006b78c88be867cc657
portatil <portatil@DESKTOP-9O6B0HP>
2024-10-16 17:18:20 +0200

    exer 4 boletincondicional corregido

--- src/boletincondicional/Ejer4.java
+++ src/boletincondicional/Ejer4.java
@@ -9,7 +9,10 @@ public class Ejer4 {
 		int num;
 		int decena;
 		int unidad;
+		String cadUnidad = "";
+		String cadDecena = "";
 		String result = "";
+		
 
 		// CREAR OBJETO SCANNER
 		Scanner sc = new Scanner(System.in);
@@ -20,67 +23,115 @@ public class Ejer4 {
 		// LEER Nº ENTERO (INT) INGRESADO EN CONSOLA
 		num = sc.nextInt();
 
+		// ME QUEDO CON LAS UNIDADES
+		unidad = num % 10;
+		
+		// ME QUEDO CON LAS DECENAS
+		decena =  num / 10;
+		
 		// DECLARAR CONDICIONAL IF -> DETERMINAR SI NUM ESTÁ DENTRO DEL RANGO
 		if (num < 1 || num > 99) { // ENTRA SI NUM ESTÁ FUERA DEL RANGO
 			System.out.println("El nº debe estar entre 1 y 99.");
-		} else if (num >= 1 && num <= 19) { // ENTRA SI NUM ESTÁ ENTRE 1 Y 29
+		} else { // ENTRA SI NUM ESTÁ ENTRE 1 Y 29
+		
 			switch (num) {  // DETERMINAR NUM DESCRITO CORRESPONDIENTE A NUM INTRODUCIDO
-			case 1 -> result = "uno";
-			case 2 -> result = "dos";
-			case 3 -> result = "tres";
-			case 4 -> result = "cuatro";
-			case 5 -> result = "cinco";
-			case 6 -> result = "seis";
-			case 7 -> result = "siete";
-			case 8 -> result = "ocho";
-			case 9 -> result = "nueve";
 			case 10 -> result = "diez";
 			case 11 -> result = "once";
 			case 12 -> result = "doce";
 			case 13 -> result = "trece";
 			case 14 -> result = "catorce";
 			case 15 -> result = "quince";
-			case 16 -> result = "dieciséis";
-			case 17 -> result = "diecisiete";
-			case 18 -> result = "dieciocho";
-			case 19 -> result = "diecinueve";
-			case 20 -> result = "veinte";
-			}
-
-		} else { // ENTRA SI EL NUM ES MAYOR DE 29
-			decena = num / 10; // CALCULA LA DECENA DEL NUM INTRODUCIDO
-			unidad = num % 10; // CALCULA LA UNIDAD DEL NUM INTRODUCIDO
-			switch (decena) { // DETERMINAR DECENA DESCRITA CORRESPONDIENTE A NUM INTRODUCIDO
-			case 2 -> result = "veinti";
-			case 3 -> result = "treinta";
-			case 4 -> result = "cuarenta";
-			case 5 -> result = "cincuenta";
-			case 6 -> result = "sesenta";
-			case 7 -> result = "setenta";
-			case 8 -> result = "ochenta";
-			case 9 -> result = "noventa";
-			}
-
-			if (unidad == 0) { // SI UNIDAD DEL NUM ES 0, NO SE HACE NADA
-			} else { // ENTRA SI UNIDAD DEL NUM ES != 0 
-				result += " y "; // AÑADE CADENA TEXTO A VARIABLE RESULT
-				switch (unidad) { // DETERMINAR UNIDAD DESCRITA CORRESPONDIENTE A NUM INTRODUCIDO
-				case 1 -> result += "uno";
-				case 2 -> result += "dos";
-				case 3 -> result += "tres";
-				case 4 -> result += "cuatro";
-				case 5 -> result += "cinco";
-				case 6 -> result += "seis";
-				case 7 -> result += "siete";
-				case 8 -> result += "ocho";
-				case 9 -> result += "nueve";
+			default -> {
+				switch(unidad) {
+				case 1 -> cadUnidad = "uno";
+				case 2 -> cadUnidad = "dos";
+				case 3 -> cadUnidad = "tres";
+				case 4 -> cadUnidad = "cuatro";
+				case 5 -> cadUnidad = "cinco";
+				case 6 -> cadUnidad = "seis";
+				case 7 -> cadUnidad = "siete";
+				case 8 -> cadUnidad = "ocho";
+				case 9 -> cadUnidad = "nueve";
+				
 				}
+				
+				switch (decena) {
+				case 1 -> cadDecena = "dieci";
+				
+				case 2 -> {
+					if (unidad == 0) {
+						cadDecena = "veinte";
+					} else {
+						cadDecena = "vienti" ;
+					}
+				}
+				
+				case 3 -> {
+					cadDecena = "treinta";
+					
+					if (unidad != 0) {
+						cadDecena = cadDecena + " y " ;					
+					}
+				}
+				
+				case 4 -> {
+					cadDecena = "cuarenta";
+					
+					if (unidad != 0) {
+						cadDecena = cadDecena + " y " ;					
+					}
+				}
+				
+				case 5 -> {
+					cadDecena = "cincuenta";
+					
+					if (unidad != 0) {
+						cadDecena = cadDecena + " y " ;					
+					}
+				}
+				
+				case 6 -> {
+					cadDecena = "sesenta";
+					
+					if (unidad != 0) {
+						cadDecena = cadDecena + " y " ;					
+					}
+				}
+				
+				case 7 -> {
+					cadDecena = "setenta";
+					
+					if (unidad != 0) {
+						cadDecena = cadDecena + " y " ;				
+					}
+				}
+				
+				case 8 -> {
+					cadDecena = "ochenta";
+					
+					if (unidad != 0) {
+						cadDecena = cadDecena + " y " ;				
+					}
+				}
+				
+				case 9 -> {
+					cadDecena = "noventa";
+					
+					if (unidad != 0) {
+						cadDecena = cadDecena + " y " ;					
+					}
+				}
+				
+				}
+			}
 			}
 		}
 		// IMPRIMIR RESULTADO PANTALLA
-		System.out.println(result);
+		System.out.println(result.equals("") ? cadDecena + cadUnidad : result);
 
 		// CERRAR OBJETO SCANNER
 		sc.close();
-	}
+
+	
+		}	
 }
