bb854fd7c7d6df73e22397c5d95ed0aa816a70e2
jl-manzano <jl.manzano@iesnervion.es>
2024-10-28 13:42:36 +0100

    update

--- src/boletinbucles/Ejer6.java
+++ src/boletinbucles/Ejer6.java
@@ -1,7 +1,26 @@
 package boletinbucles;
 
+import java.util.Scanner;
+
 public class Ejer6 {
 	public static void main(String[] args) {
+		
+		int num;
+		int crecer;
+		int decrecer;
+		
+		// crear scanner
+		Scanner sc = new Scanner(System.in);
+
+		// pedir num a usuario
+		System.out.println("Introduzca un nº: ");
+
+		// leer num introducido por usuario
+		num = sc.nextInt();
+		
+		for(int i = 0, j = num--; i < num; i++, j--) {
+			
+		}
 		
 	}
 }
